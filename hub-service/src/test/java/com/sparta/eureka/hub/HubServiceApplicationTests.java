d7e974e2f7840f750ca8b0cfafa53b2f0fe51bad
earthkid0 <ghs1541@naver.com>
2025-03-13 16:46:52 +0900

    feat/hub-service 기본 설정

---
+++ hub-service/src/test/java/com/sparta/eureka/hub/HubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.eureka.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
