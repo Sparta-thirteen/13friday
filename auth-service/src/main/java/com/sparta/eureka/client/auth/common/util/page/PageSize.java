66a1fe2046602d492a1b7ffc7dc8a946238c6a6d
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-03-21 22:59:13 +0900

    feat: GlobalException 및 페이징 관련 설정

---
+++ auth-service/src/main/java/com/sparta/eureka/client/auth/common/util/page/PageSize.java
@@ -0,0 +1,22 @@
+package com.sparta.eureka.client.auth.common.util.page;
+
+import java.util.EnumSet;
+import lombok.Getter;
+
+@Getter
+public enum PageSize {
+  DEFAULT(10),
+  THIRTY(30),
+  FIFTY(50);
+
+  private int size;
+  PageSize(int size) {
+    this.size = size;
+  }
+
+  public static boolean isValidSize(int size) {
+    return EnumSet.allOf(PageSize.class)
+        .stream()
+        .anyMatch(ps -> ps.getSize() == size);
+  }
+}
